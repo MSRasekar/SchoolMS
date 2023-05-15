fc507470295654dc143ccc1d71f20e32cfd0de9b
MSRasekar <msrasekar@gmail.com>
2023-05-15 16:06:29 +0530

    added institute file in testing

---
+++ Institute.java
@@ -0,0 +1,27 @@
+public class Institute {
+
+    private String instituteName;
+    private String address;
+
+    public String getInstituteName() {
+        return instituteName;
+    }
+
+    public void setInstituteName(String instituteName) {
+        this.instituteName = instituteName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Institute [instituteName=" + instituteName + ", address=" + address + "]";
+    }
+
+}
